48d1b8a7a82754d7b03bc57046e5595a2043e5af
Jeff Trimmer <trimmerjeff@gmail.com>
2018-07-09 12:45:34 -0700

    Added products component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ProductListComponent} from './products/product-list.component';
+import {FormsModule} from "@angular/forms";
+import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
